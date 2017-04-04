0a3f85ad0a8ab92a5042fcf51b2d38231ef2d735
Based God <mxa131730@tamu.edu>
2017-04-04 13:15:22 -0500

    added the intial pictures and refactored Markov Chain

--- backend/markov/polls/models.py
+++ backend/markov/polls/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Story(models.Model):
+    story_name = models.CharField(max_length = 50)
+    user_name  = models.CharField(max_length = 50)
+
+class Frame(models.Model):
+    background_image  = models.CharField(max_length = 50)
+    story             = models.ForeignKey(Story)
+
+class Character(models.Model):
+    name       = models.CharField(max_length = 50)
+    direction  = models.CharField(max_length = 5)
+    position   = models.CharField(max_length = 5)
+    text       = models.CharField(max_length = 70)
+    frame      = models.ForeignKey(Frame)
