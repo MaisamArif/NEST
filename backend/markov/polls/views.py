114d71624c57bd99397a96871163158f33fdd485
Ubuntu <ubuntu@ip-172-31-3-247.us-west-2.compute.internal>
2017-05-01 17:41:00 +0000

    updated db

--- backend/markov/polls/views.py
+++ backend/markov/polls/views.py
@@ -273,7 +273,7 @@ def Continue(request):
 
     #FRAMES
     for elem in range(start - 1, end):
-        frame = Frame(background_image = 'https://responsivewebcomics.me"+'/'+"mint_background.jpg', frame_id = elem+1, story = s)
+        frame = Frame(background_image = 'https://responsivewebcomics.me" + "/mint_background.jpg', frame_id = elem+1, story = s)
         frame.save()
 
         #change to this if we start doing more than 2 characters a frame
